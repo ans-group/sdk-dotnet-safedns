bb1009e8ef6e9cb83ac13e03d8ac82e79dd365f1
Lee Farrell <lee.farrell@ukfast.co.uk>
2020-06-04 18:49:06 +0100

    add account settings operations

---
+++ UKFast.API.Client.SafeDNS.Tests/Operations/SettingsOperationsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Threading.Tasks;
+using UKFast.API.Client.SafeDNS.Models;
+using UKFast.API.Client.SafeDNS.Operations;
+
+namespace UKFast.API.Client.SafeDNS.Tests.Operations
+{
+    [TestClass]
+    public class SettingsOperationsTests
+    {
+        [TestMethod]
+        public async Task GetSettingsAsync_ExpectedResult()
+        {
+            IUKFastSafeDNSClient client = Substitute.For<IUKFastSafeDNSClient>();
+            client.GetAsync<Settings>("/safedns/v1/settings").Returns(new Settings()
+            {
+                ID = 123
+            });
+
+            var ops = new SettingsOperations<Settings>(client);
+            var settings = await ops.GetSettingsAsync();
+
+            Assert.AreEqual(123, settings.ID);
+        }
+    }
+}
